05744ea6c3f3e199bb79a8527f118c68caa47d72
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-06 12:43:45 +0100

    Add init, recieve and transmit char functions. Main in echo

--- src/bluetooth.h
+++ src/bluetooth.h
@@ -1,5 +1,14 @@
 #ifndef __BLUETOOTH_H__
 #define __BLUETOOTH_H__
 
+#define FOSC 13000000 // Clock Speed
+#define BAUD 38400
+#define MYUBRR FOSC/16/BAUD-1
+
+void Bluetooth_Init(unsigned int ubrr);
+
+unsigned char Bluetooth_Receive(void);
+
+void Bluetooth_Transmit(unsigned char data);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,13 @@
+#include <util/delay.h>
+
+#include "bluetooth.h"
 
 void main() {
+  Bluetooth_Init(MYUBRR);
+
+  while(1){
+    char c = Bluetooth_Receive();
+    Bluetooth_Transmit(c);
+  }
   return;
 }
