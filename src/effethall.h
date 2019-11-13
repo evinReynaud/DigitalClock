bcf30e30efd910af7e174fe1caa1bc69f4bdfee1
laelerandy <llerandy@ensc.fr>
2019-11-13 21:57:05 +0100

    effethall led blink when we met the magnet

---
+++ src/effethall.h
@@ -0,0 +1,7 @@
+#ifndef __EFFETHALL_H__
+#define __EFFETHALL_H__
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,29 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "effethall.h"
+#include "spi.h"
 
-void main() {
-  return;
+ISR(INT0_vect)
+{
+  SPI_MasterTransmit(0b1000000000000000);
+}
+
+void main()
+{
+
+  // Change pin 0 on bus D to an input by changing bit 0 to zero
+  DDRD &= ~(1 << PIN0);
+
+  // Allow external interrupt 0
+  EIMSK |= (1 << INT0);
+
+  // Allow global interrupts
+  sei();
+  SPI_MasterInit();
+
+  while (TRUE)
+  {
+
+    SPI_MasterTransmit(0b1111110000110001); // the last bit is the led that is the closest of the center
+  }
 }
