89e738949c1fc22a12664c78e8807f5f2388e662
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-13 15:19:04 +0100

    Code suposed to be working

--- src/spi.c
+++ src/spi.c
@@ -5,8 +5,9 @@
 
 void SPI_MasterInit(void)
 {
-    DDRE |= _BV(PE4);
-    DDRE &= ~_BV(PE5);
+    // DDRE |= _BV(PE4);
+    // DDRE |= _BV(PE5);
+    DDRE = (1 << DDE4) | (1 << DDE5);
     /* Set MOSI and SCK output, all others input */
     DDRB = (1 << DDB2) | (1 << DDB1);
     /* Enable SPI, Master, set clock rate fck/16 */
