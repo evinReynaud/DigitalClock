72edbb621f902bcee5d8dc791b758a3d8a916f61
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-14 10:53:16 +0100

    New bluetooth files

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -6,26 +6,46 @@
 void Bluetooth_Init(unsigned int ubrr)
 {
   //Set baud rate
-  UBRR0H = (unsigned char)(ubrr>>8);
+  UBRR0H = (unsigned char)(ubrr >> 8);
   UBRR0L = (unsigned char)ubrr;
   // Enable receiver and transmitter
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
   // Set frame format: 8data, 2stop bit
-  UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+  UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
-unsigned char Bluetooth_Receive(void)
+char Bluetooth_Receive_Char()
 {
   // Wait for data to be received
-  while (!(UCSR0A & (1<<RXC0)));
+  while (!(UCSR0A & (1 << RXC0)))
+    ;
   // Get and return received data from buffer
   return UDR0;
 }
 
-void Bluetooth_Transmit(unsigned char data)
+void Bluetooth_Receive(char* buff)
+{
+  char c;
+  do {
+    c = Bluetooth_Receive_Char();
+    *buff = c;
+    buff++;
+  } while (c != '\n');
+  *buff = '\0';
+}
+
+void Bluetooth_Transmit_Char(char data)
 {
   // Wait for empty transmit buffer
-  while (!(UCSR0A & (1<<UDRE0)));
+  while (!(UCSR0A & (1 << UDRE0)))
+    ;
   // Put data into buffer, sends the data
   UDR0 = data;
+}
+
+void Bluetooth_Transmit(char* data)
+{
+  while(*data != '\0') {
+    Bluetooth_Transmit_Char(*data++);
+  }
 }
--- src/bluetooth.h
+++ src/bluetooth.h
@@ -3,12 +3,18 @@
 
 #define FOSC 13000000 // Clock Speed
 #define BAUD 38400
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR FOSC / 16 / BAUD - 1
+#define TRUE 1
+#define FALSE 0
 
 void Bluetooth_Init(unsigned int ubrr);
 
-unsigned char Bluetooth_Receive(void);
+void Bluetooth_Receive(char* buff);
 
-void Bluetooth_Transmit(unsigned char data);
+char Bluetooth_Receive_Char();
+
+void Bluetooth_Transmit(char* data);
+
+void Bluetooth_Transmit_Char(char data);
 
 #endif
