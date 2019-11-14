126d55e7214ce545660565dc161d609d1145e8b7
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-14 11:28:09 +0100

    Clean files and split them between hardware interface and logic

--- src/main.c
+++ src/main.c
@@ -1,28 +1,10 @@
-#include <avr/io.h>
-#include <stdlib.h>
-#include <util/delay.h>
-#include <string.h>
-
-#include "spi.h"
-#include "bluetooth.h"
+#include "leds.h"
 
 void main(void)
 {
-  SPI_MasterInit();
-  //Bluetooth_Init(MYUBRR);
-  while (TRUE)
+  leds_init();
+  while(1)
   {
-
-    char buffer[256];
-    Bluetooth_Receive(buffer);
-    int num;
-    if(strlen(buffer) >= 16)
-      num = (int) strtol(buffer, NULL, 2);
-    else
-      num = (int) strtol(buffer, NULL, 16);
-    char c1 = (char) (num >> 8);
-    char c2 = (char) num;
-    SPI_MasterTransmit(num);
-    Bluetooth_Transmit(buffer);
+    leds_on(0b1000000011111110);
   }
 }
