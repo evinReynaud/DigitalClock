b283f306f3e6c9bad26cef039bf1257f9f7ce36a
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-27 11:11:40 +0100

    Change nb of cycles for interrupt

--- src/timer.c
+++ src/timer.c
@@ -15,9 +15,9 @@ void timer_init(void)
 void timer_start(void)
 {
     timer_init();
-    //OCR1  = 1625 ; /* overflow in OCR1*8 clock cycles */
+    //OCR1  = 1624 ; /* overflow in OCR1*8 clock cycles */
     OCR1AH = 0b00000110;
-    OCR1AL = 0b01011001;
+    OCR1AL = 0b01011000;
     TIMSK |= _BV(TOIE1); /* enable overflow interrupt */
     /* start timer clock */
     //TCCR1B &= ~(_BV(CS10)|_BV(CS11)|_BV(CS12));
