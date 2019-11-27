162d7647c678535a761a3d9fdac26c8982d3633e
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-11-27 12:20:56 +0100

    First skeleton

---
+++ src/display_analog.c
@@ -0,0 +1,16 @@
+#include "display_analog.h"
+
+int hours = 0;
+int minutes = 0;
+int seconds = 0;
+
+int get_arc_min()
+{
+  return 0;
+}
+
+void display_strip()
+{
+  int arc_minutes = get_arc_min();
+  
+}
---
+++ src/display_analog.h
@@ -0,0 +1,6 @@
+#ifndef __DISPLAY_ANALOG_H__
+#define __DISPLAY_ANALOG_H__
+
+void display_strip();
+
+#endif
