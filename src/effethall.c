21269bcde915002120b5374fb2abb4dc40906242
evinReynaud <44316037+evinReynaud@users.noreply.github.com>
2019-12-10 10:11:40 +0100

    first modifications

--- src/effethall.c
+++ src/effethall.c
@@ -1,7 +1,14 @@
-// avr header for the register
+#include <avr/interrupt.h>
+#include <avr/io.h>
 
 //#include "spi.h"
+
 #include "effethall.h"
+
+ISR(INT0_vect)
+{
+  etat = 1;
+}
 
 void effethall_init()
 {
@@ -10,4 +17,24 @@ void effethall_init()
   DDRD &= ~(1 << PIN0);
   // Allow external interrupt 0
   EIMSK |= (1 << INT0);
+}
+
+void check_effethall()
+{
+  if( etat == 1)
+  {
+    if(follow == 0)
+    {
+      follow = 1;
+      TIM16_ReadTCNT3();
+      //bluetooth_transmit_uint16(countPerTour);
+      TIM16_WriteTCNT3(0);
+      //bluetooth_ln();
+    }
+    if( TCNT3 > 100)
+    {
+      etat = 0;
+      follow = 0;
+    }
+  }
 }
--- src/effethall.h
+++ src/effethall.h
@@ -1,11 +1,9 @@
 #ifndef __EFFETHALL_H__
 #define __EFFETHALL_H__
 
-#include <avr/interrupt.h>
-#include <avr/io.h>
 #include "effethall.h"
-#define TRUE 1
-#define FALSE 0
 
 void effethall_init();
+void check_effethall();
+
 #endif
